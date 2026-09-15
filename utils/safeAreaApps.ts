6b8fe6814d1c2b56bcb163e99114aa5a19423add
whisletter <eleadam@163.com>
2026-09-16 01:26:57 +0800

    Update safeAreaApps.ts

--- utils/safeAreaApps.ts
+++ utils/safeAreaApps.ts
@@ -44,6 +44,7 @@ export const SELF_SAFE_AREA_APPS: ReadonlySet<AppID> = new Set<AppID>([
     AppID.WorldHome,
     AppID.CharCreatorDev,
     AppID.SpecialMoments,
+    AppID.YuZhou,
 ]);
 
 // 外壳是否需要替这个 App 让出安全区：不在自理名单里的才需要。
